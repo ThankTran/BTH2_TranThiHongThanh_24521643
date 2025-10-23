066c29bf769a29e9195dd9f17c14f42712579bfc
thanhtran <hongthanh3aqn@gmail.com>
2025-10-23 22:27:27 +0700

    cm b4

--- Bai04/Program.cs
+++ Bai04/Program.cs
@@ -18,7 +18,7 @@ namespace Bai04
 
                 for (int j = i + 1; j < n; j++)
                 {
-                    if (Fraction.Compare(array[j], array[minIndex]) < 0)
+                    if (Fraction.Compare(array[j],array[minIndex])<0)
                     {
                         minIndex = j;
                     }
@@ -35,27 +35,31 @@ namespace Bai04
             Fraction a = new Fraction();
             Fraction b = new Fraction();
 
-            Console.WriteLine("Enter two fractions to perform operations: ");
-            Console.WriteLine("Enter fraction the first fraction (numerator and denominator): ");
+            Console.WriteLine("Enter two fractions: ");
+            Console.WriteLine("Enter fraction the first fraction: ");
             a.Nhap();
-            Console.WriteLine("Enter fraction the second fraction (numerator and denominator): ");
+            Console.WriteLine("Enter fraction the second fraction: ");
             b.Nhap();
 
-            Console.WriteLine("Sum of two fractions: " + (a + b));
-            Console.WriteLine("Difference of two fractions: " + (a - b));
-            Console.WriteLine("Product of two fractions: " + (a * b));
-            Console.WriteLine("Quotient of two fractions: " + (a / b));
+            Console.WriteLine
+                ("Sum of two fractions: " + (a + b));
+            Console.WriteLine
+                ("Difference of two fractions: " + (a - b));
+            Console.WriteLine
+                ("Product of two fractions: " + (a * b));
+            Console.WriteLine
+                ("Quotient of two fractions: " + (a / b));
 
             int n;
             while (true)
             {
-                Console.Write("Enter the number of fractions to sort: ");
+                Console.Write("Enter the number of fractions: ");
                 string inputN = Console.ReadLine() ?? string.Empty;
                 if (int.TryParse(inputN, out n) && n > 0)
                 {
                     break;
                 }
-                Console.WriteLine("Please enter a positive integer for N.");
+                Console.WriteLine("Please enter a positive number.");
             }
 
             Fraction[] fractions = new Fraction[n];
@@ -74,11 +78,6 @@ namespace Bai04
                 Console.Write(frac + "   ");
             }
             Console.WriteLine("\n");
-
-
         }
-
-
-
     }
 }
